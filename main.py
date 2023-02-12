f9ce12d82e750813f11387a5928c4b4935388ede
Murad <muradmustafayev03@gmail.com>
2023-02-12 14:56:12 +0400

    fine tune regressors & voting regressor

--- main.py
+++ main.py
@@ -1,20 +1,30 @@
-from sklearn.ensemble import RandomForestRegressor
+from sklearn.ensemble import RandomForestRegressor, GradientBoostingRegressor, VotingRegressor
 from sklearn.metrics import mean_squared_error
+import matplotlib.pyplot as plt
 import pandas as pd
+import numpy as np
 from util import format_df
-
 
 df = format_df(pd.read_csv('data/modified_train_binaaz.csv'))
 
 pivot = int(len(df.index) * 0.8)
 
-x_train = df[['floor', 'floors', 'area', 'new_building', 'longitude', 'latitude']][:pivot]
-x_test = df[['floor', 'floors', 'area', 'new_building', 'longitude', 'latitude']][pivot:]
+x_train = df[[col for col in df.keys() if col != 'price']][:pivot]
+x_test = df[[col for col in df.keys() if col != 'price']][pivot:]
 y_train = df['price'][:pivot]
 y_test = df['price'][pivot:]
 
-reg = RandomForestRegressor()
-reg.fit(x_train, y_train)
+reg1 = RandomForestRegressor(n_estimators=100, max_depth=180)
+reg2 = GradientBoostingRegressor(n_estimators=190, max_depth=5)
 
-print(reg.score(x_test, y_test))
-print(mean_squared_error(y_test, reg.predict(x_test)))
+er = VotingRegressor([('lf', reg1), ('gb', reg2)], weights=[0.7, 0.3])
+er.fit(x_train, y_train)
+
+print(er.score(x_test, y_test))
+print(np.sqrt(mean_squared_error(y_test, er.predict(x_test))))
+
+
+plt.scatter(x_test.index, y_test, marker='x')
+plt.scatter(x_test.index, er.predict(x_test), marker='x')
+plt.show()
+
--- util.py
+++ util.py
@@ -13,13 +13,32 @@ def to_standard(series):
 
 def format_df(df: pd.DataFrame):
     df['new_building'] = [int(value == 'Yeni tikili') for value in df['category']]
+    df['hypothec'] = [int(value == 'var') for value in df['hypothec']]
+    df['repairs'] = [int(value == 'var') for value in df['repairs']]
+    df['kupcha'] = [int(value == 'var') for value in df['kupcha']]
     df['area'] = [float(value[:-3]) for value in df['area']]
     df['floors'] = [int(s.split(' / ')[1]) for s in df['floor']]
     df['floor'] = [int(s.split(' / ')[0]) for s in df['floor']]
 
-    df = df[[
-        'latitude', 'longitude', 'floor', 'floors', 'area', 'new_building', 'price'
-    ]]
+    df['urgent'] = [
+        int(
+            'təci̇li̇' in str(text).lower() or
+            'teci̇li̇' in str(text).lower() or
+            'təcili' in str(text).lower() or
+            'tecili' in str(text).lower()
+        ) for text in df['description']
+    ]
+
+    if 'price' in df.keys():
+        df = df[[
+            'latitude', 'longitude', 'floor', 'floors', 'rooms', 'hypothec', 'repairs', 'kupcha', 'area',
+            'new_building', 'urgent', 'price'
+        ]]
+    else:
+        df = df[[
+            'latitude', 'longitude', 'floor', 'floors', 'rooms', 'hypothec', 'repairs', 'kupcha', 'area',
+            'new_building', 'urgent',
+        ]]
 
     for key in df.keys():
         if key != 'price':
